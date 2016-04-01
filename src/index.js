eea0a25551476b1ba98f33d3cf2104c919aaa229
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-04-01 15:06:35 +0530

    Use pure JavaScript jwt-js as the jwt implmentation. Otherwise, you had to include NodeJS polyfills which in return cause some issues

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken';
+import { decodeToken } from 'jwt-js';
 import HttpTransport from 'lokka-transport-http';
 
 // if the token expires within the next N ms
@@ -113,7 +113,7 @@ export default class Transport {
 
       // assuming the token has an expiration time
       // TODO handle tokens without expiration times
-      const payload = jwt.decode(token);
+      const payload = decodeToken(token).payload;
       if (!payload || !payload.exp) {
         throw new Error('invalid token');
       }
